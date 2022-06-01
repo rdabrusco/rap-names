9108371a7bb74e1be0737c3d83a66249d72555e3
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2022-06-01 14:25:46 -0400

    add images to rappers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-let button = document.querySelector('button')
+const button = document.querySelector('button')
+const born = document.querySelector('h2')
+const pic = document.querySelector('img')
 
-// button.addEventListener('click', )
+button.addEventListener('click', getName )
+document.querySelector('input').addEventListener('keypress', e=>{
+    if(e.key === 'Enter'){
+        e.preventDefault()
+        getName()
+    }
+})
+
+async function getName(){
+    const rapperName = document.querySelector('input').value
+    try{
+         fetch(`https://rap-names-api.herokuapp.com/api/${rapperName}`)
+         .then(res => res.json())
+         .then(data => {
+             console.log(data)
+             born.textContent = `Birth Name: ${data.birthName}`
+             pic.src = data.img
+         })
+    }
+    catch(err){
+        console.error(err)
+    }
+   
+
+}
--- server.js
+++ server.js
@@ -10,17 +10,20 @@ const rappers = {
     '21 savage':{
         'age': 29,
         'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England'
+        'birthLocation': 'London, England',
+        'img': 'https://media.pitchfork.com/photos/5c5c555fb343c831c1730802/4:3/w_524,h_393,c_limit/21%20Savage%202.png'
     },
     'chance the rapper':{
         'age': 29,
         'birthName': 'Chancelor Johnathan Bennett',
-        'birthLocation': 'Chicago, Illinois'
+        'birthLocation': 'Chicago, Illinois',
+        'img': 'https://www.biography.com/.image/t_share/MTQ3Mzg3OTQyMTQ5NTYzNTg1/chance_the_rapper_photo_by_matthew_eisman_getty_images_entertainment_getty_170863100.jpg'
     },
     'dylan':{
         'age': 'Dylan',
         'birthName': 'Dylan',
-        'birthLocation': 'Dylan'
+        'birthLocation': 'Dylan',
+        'img': 'https://miro.medium.com/max/1024/1*p_axiPYUOkeYY0usKNhd_A.png'
     }
     
 }
@@ -39,7 +42,8 @@ app.get('/api/:rapperName', (req, res)=> {
     
 })
 
-// app.get('/js/main.js', (req, res))
+
+
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on port ${PORT}, you better go catch it!`)
