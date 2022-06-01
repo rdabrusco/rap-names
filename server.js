4b1a01026a84c96cba89d215484507f48a7fb533
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2022-05-31 21:53:32 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage':{
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Johnathan Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan':{
+        'age': 'Dylan',
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    }
+    
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (req, res)=> {
+    const rappersName = req.params.rapperName.toLowerCase()
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    }else{
+        res.json(rappers['dylan'])
+    }
+    
+})
+
+app.get('/js/main.js', (req, res))
+
+app.listen(PORT, ()=>{
+    console.log(`The server is running on port ${PORT}, you better go catch it!`)
+})
